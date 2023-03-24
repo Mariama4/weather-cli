31cd9aa6a945862ca5c62891b66abe923f14d2e8
mariama4 <mariama4@mail.ru>
2023-03-24 14:55:40 +0700

    created a storage service to save the user token

---
+++ services/storage.service.js
@@ -0,0 +1,38 @@
+import { homedir } from "os";
+import { join } from "path";
+import { promises } from "fs";
+
+const filePath = join(homedir(), "weather-data.json");
+
+const readFile = async (path) => {
+  const file = await promises.readFile(path);
+  return JSON.parse(file);
+};
+
+const isExist = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const getKeyValue = async (key) => {
+  if (await isExist(filePath)) {
+    let data = await readFile(filePath);
+    return data[key];
+  }
+  return undefined;
+};
+
+const saveKeyValue = async (key, value) => {
+  let data = {};
+  if (await isExist(filePath)) {
+    data = await readFile(filePath);
+  }
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data));
+};
+
+export { saveKeyValue, getKeyValue };
